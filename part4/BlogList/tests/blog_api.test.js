1f44e950e7ee966224a06e606760f3d63c8b6069
Konstantin <95539196+EalsyCoD@users.noreply.github.com>
2022-01-20 18:44:05 +0300

    Exercises 3.9
    Exercises 3.9

--- part4/BlogList/tests/blog_api.test.js
+++ part4/BlogList/tests/blog_api.test.js
@@ -15,14 +15,21 @@ beforeEach(async () => {
   })
 
 
-
-test('blogs are returned as json', async () => {
-  const response = await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-    expect(response.body).toHaveLength(helper.initialBlogs.length)
-},10000)
+describe('api testing', ()=> {
+    test('blogs are returned as json', async () => {
+        const response = await api
+          .get('/api/blogs')
+          .expect(200)
+          .expect('Content-Type', /application\/json/)
+          expect(response.body).toHaveLength(helper.initialBlogs.length)
+      },10000)
+       
+test('unique identifier', async () => {
+    const result = await api.get('api/blogs').expect(200)
+    expect(result.body[0].id).tobeDefined()
+})
+})
+  
 
 afterAll(() => {
   mongoose.connection.close()
