9f104ccb0d7a10426385073ca1ed473b035fa360
Konstantin <95539196+EalsyCoD@users.noreply.github.com>
2022-01-20 17:40:01 +0300

    Add files via upload

--- part4/BlogList/utils/list_helper.js
+++ part4/BlogList/utils/list_helper.js
@@ -2,6 +2,27 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  let sum = 0
+  blogs.map((blog) => (sum += blog.likes))
+  return sum
+}
+
+const favoriteBlog = (blogs) => {
+  let maxLikes = 0
+  let BlogObject = null
+
+  blogs.map((blog) => {
+    if (blog.likes > maxLikes) {
+      maxLikes = blog.likes
+      BlogObject = blog
+}
+  })
+  return BlogObject
+}
+
 module.exports = {
    dummy,
+   totalLikes,
+   favoriteBlog,
 }
