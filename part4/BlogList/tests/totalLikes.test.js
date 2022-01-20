d7b82f7c303123f79b258dc8c0345b48628d7230
Konstantin <95539196+EalsyCoD@users.noreply.github.com>
2022-01-20 17:41:29 +0300

    Add files via upload

--- part4/BlogList/tests/totalLikes.test.js
+++ part4/BlogList/tests/totalLikes.test.js
@@ -60,7 +60,7 @@ const listHelper = require('../utils/list_helper')
           url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
           likes: 5,
           __v: 0
-        }
+        },
       ]
       describe('total likes', () => {
         test("of empty list is zero" , () => {
@@ -69,9 +69,24 @@ const listHelper = require('../utils/list_helper')
           expect(result).toBe(0)
         })
 
-
     test('when list has only one blog, equals the likes of that', () => {
       const result = listHelper.totalLikes(listWithOneBlog)
       expect(result).toBe(5)
     })
+   test('of a bigger list is calculated right', () => {
+     const result = listHelper.totalLikes(blogs)
+     expect(result).toBe(36)
+   })
+    test('favorite blog', () => {
+      const result = listHelper.totalLikes(blogs)
+      expect(result).toEqual({
+          _id: "5a422b891b54a676234d17fa",
+          title: "First class tests",
+          author: "Robert C. Martin",
+          url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+          likes: 10,
+          __v: 0
+        
+      })
+    }) 
   })
