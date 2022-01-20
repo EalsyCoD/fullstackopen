749b240582f68179d0e514b58a610d234b711b1e
Konstantin <95539196+EalsyCoD@users.noreply.github.com>
2022-01-20 20:15:46 +0300

    Add files via upload

--- part4/BlogList/models/blog.js
+++ part4/BlogList/models/blog.js
@@ -1,24 +1,22 @@
 const mongoose = require('mongoose')
 
-
-
-
-const blogSchema = new mongoose.Schema({
-  content: {
-    type: String,
-    required: true,
-    minlength: 5
+const blogSchema = mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
   },
-  date: Date,
-  important: Boolean,
 })
 
 blogSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
+    returnedObject.id = returnedObject._id
     delete returnedObject._id
     delete returnedObject.__v
-  }
+  },
 })
 
 module.exports = mongoose.model('Blog', blogSchema)
