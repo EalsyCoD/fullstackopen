f8bb3ca2519878c679f3d8586eb6392cb69a12c5
Konstantin <95539196+EalsyCoD@users.noreply.github.com>
2022-01-20 20:15:26 +0300

    Add files via upload

--- part4/BlogList/tests/blog_api.test.js
+++ part4/BlogList/tests/blog_api.test.js
@@ -3,33 +3,62 @@ const supertest = require('supertest')
 const app = require('../app')
 const api = supertest(app)
 const helper = require('./test_helper')
-
 const Blog = require('../models/blog')
-
 beforeEach(async () => {
-    await Blog.deleteMany({})
-    for (let blog of helper.initialBlogs) {
-      let blogObject = new Blog(blog)
-      await blogObject.save()
+  await Blog.deleteMany({})
+  for (let blog of helper.initialBlogs) {
+    let blogObject = new Blog(blog)
+    await blogObject.save()
+  }
+})
+describe('api testing', () => {
+  test('blogs are returned as json', async () => {
+    const response = await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+    expect(response.body).toHaveLength(helper.initialBlogs.length)
+  }, 100000)
+  test('verifies that the unique identifier property of the blog posts is named id', async () => {
+    const result = await api.get('/api/blogs').expect(200)
+    expect(result.body[0].id).toBeDefined()
+  })
+  test('POST request saves blog to MongoDB', async () => {
+    const newBlog = {
+      title: 'Api testing',
+      author: 'EalsyCoD',
+      url: 'github.com/EalsyCoD',
+      likes: 1,
     }
+    await api.post('/api/blogs').send(newBlog)
+    const blogs_ = await helper.blogsInDb()
+    expect(blogs_).toHaveLength(helper.initialBlogs.length + 1)
+    const content = blogs_.map((x) => x.author)
+    expect(content).toContain('EalsyCoD')
+  })
+  test('If the likes property is missing, it will default to 0 ', async () => {
+    const newBlog = {
+      title: 'Test for likes',
+      author: 'EalsyCoD',
+      url: 'github.com/EalsyCoD',
+    }
+    await api.post('/api/blogs').send(newBlog)
+    const blogs_ = await helper.blogsInDb()
+    const content = await blogs_.find((blog) => blog.title === newBlog.title)
+    expect(content.likes).toBe(0)
   })
 
+  test('verifies that if the title and url properties are missing from the request data', async () => {
+    const newBlog = {
+      author: 'EalsyCoD',
+    }
 
-describe('api testing', ()=> {
-    test('blogs are returned as json', async () => {
-        const response = await api
-          .get('/api/blogs')
-          .expect(200)
-          .expect('Content-Type', /application\/json/)
-          expect(response.body).toHaveLength(helper.initialBlogs.length)
-      },10000)
-       
-test('unique identifier', async () => {
-    const result = await api.get('api/blogs').expect(200)
-    expect(result.body[0].id).tobeDefined()
+    await api.post('/api/blogs').send(newBlog).expect(400)
+
+    const blogs_ = await helper.blogsInDb()
+    expect(blogs_).toHaveLength(helper.initialBlogs.length)
+  })
 })
-})
-  
 
 afterAll(() => {
   mongoose.connection.close()
