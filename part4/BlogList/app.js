eb38067b9abc6b50bc733ca4d381b1db356645d8
Konstantin <95539196+EalsyCoD@users.noreply.github.com>
2022-01-23 01:29:58 +0300

    Exercises 5.17-5.21

--- part4/BlogList/app.js
+++ part4/BlogList/app.js
@@ -1,33 +1,40 @@
-const config = require('./utils/config')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('express-async-errors')
-const blogsRouter = require('./controllers/blogs')
-const middleware = require('./utils/middleware')
-const logger = require('./utils/logger')
+const config = require("./utils/config");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("express-async-errors");
+const blogsRouter = require("./controllers/blogs");
+const middleware = require("./utils/middleware");
+const logger = require("./utils/logger");
+const loginRouter = require("./controllers/login");
+const usersRouter = require("./controllers/users");
 
+logger.info("connecting to", config.MONGODB_URI);
 
-
-
-logger.info('connecting to', config.MONGODB_URI)
-
-mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose
+  .connect(config.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
   .then(() => {
-    logger.info('connected to MongoDB')
+    logger.info("connected to MongoDB");
   })
   .catch((error) => {
-    logger.error('error connection to MongoDB:', error.message)
-  })
+    logger.error("error connecting to MongoDB:", error.message);
+  });
 
-app.use(cors())
-app.use(express.static('build'))
-app.use(express.json())
-app.use(middleware.requestLogger)
+app.use(cors());
+app.use(express.static("build"));
+app.use(express.json());
+app.use(middleware.requestLogger);
 
-app.use('/api/blogs', blogsRouter)
+app.use("/api/login", loginRouter);
+app.use("/api/blogs", blogsRouter);
 
-app.use(middleware.unknownEndpoint)
-app.use(middleware.errorHandler)
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
-module.exports = app
+module.exports = app;
