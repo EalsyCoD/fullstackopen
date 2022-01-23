c1d62e54259fc47b8ccfe00961136bac3f3be729
Konstantin <95539196+EalsyCoD@users.noreply.github.com>
2022-01-23 22:51:20 +0300

    Exercises 5.1-5.4

---
+++ part5/bloglist-frontend/src/components/BlogForm.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+
+const BlogForm = ({ createBlog }) => {
+  const [newBlog, setNewBlog] = useState("");
+
+  const handleChange = (event) => {
+    setNewBlog(event.target.value);
+  };
+
+  const addBlog = (event) => {
+    event.preventDefault();
+    createBlog({
+      content: newBlog,
+      important: Math.random() > 0.5,
+    });
+
+    setNewBlog("");
+  };
+
+  return (
+    <div>
+      <h2>Create a new blog</h2>
+
+      <form onSubmit={addBlog}>
+        <input value={newBlog} onChange={handleChange} />
+        <button type="submit">save</button>
+      </form>
+    </div>
+  );
+};
+
+export default BlogForm;
