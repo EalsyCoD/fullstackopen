7eb5902ac38e6d050a55e6e319484f50aa832c30
Konstantin <95539196+EalsyCoD@users.noreply.github.com>
2022-01-26 02:12:50 +0300

    Exercises 5.17-5.20

--- part5/bloglist-frontend/cypress/integration/blog_app.spec.js
+++ part5/bloglist-frontend/cypress/integration/blog_app.spec.js
@@ -2,27 +2,57 @@ describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
     const user ={
-      name: 'cod',
-      username: 'CoD',
-      password: 'ealsy'
+      name: 'test',
+      username: 'test',
+      password: 'test'
     }
     cy.request('POST', 'http://localhost:3003/api/users/', user)
     cy.visit('http://localhost:3000')
   })
 
   it('Login form is shown', function() {
-    cy.contains('login').click
-    cy.get('input:first').type('CoD')
-    cy.get('input:last').type('ealsy')
-    cy.get('#login-button').click()
-
-    cy.contains('cod logged in')
+    cy.contains('username')
+    cy.contains('password')
+    cy.contains('login')
   })
-  it('fails with wrong credentials', function(){
-    cy.contains('login').click()
-    cy.get('input:first').type('cid')
-    cy.get('input:last').type('retert')
-    cy.get('#login-button').click()
-    cy.contains('Wrong credentials')
+  describe('Login', function(){
+    it('succeeds with correct credentials', function(){
+      cy.contains('login').click()
+      cy.get('#username').type('test')
+      cy.get('#password').type('test')
+      cy.get('#login-button').click()
+
+      cy.contains('test logged in')
+      cy.contains('Logout').click()
+    })
+
+
+    it('fails with wrong credentials', function () {
+      cy.contains('login').click()
+      cy.get('#username').type('test')
+      cy.get('#password').type('t')
+      cy.get('#login-button').click()
+
+      cy.get('#notification').should('contain', 'Wrong credentials')
+    })
+  })
+  describe('when logged in', function(){
+    beforeEach(function () {
+      cy.contains('login').click()
+      cy.get('#username').type('test')
+      cy.get('#password').type('test')
+      cy.get('#login-button').click()
+    })
+
+    it('a new blog can be created', function () {
+      cy.contains('create new Blog').click()
+      cy.get('#title').type('test Blog')
+      cy.get('#author').type('test')
+      cy.get('#url').type('http://test.com')
+      cy.get('#create').click()
+      cy.contains('new blog not added')
+    })
   })
 })
+
+
