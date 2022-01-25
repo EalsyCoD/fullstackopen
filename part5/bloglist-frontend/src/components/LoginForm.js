6142294202e5e0fcfc9e8bac2bc4656b698bffd2
Konstantin <95539196+EalsyCoD@users.noreply.github.com>
2022-01-25 17:13:03 +0300

    Add files via upload

--- part5/bloglist-frontend/src/components/LoginForm.js
+++ part5/bloglist-frontend/src/components/LoginForm.js
@@ -1,27 +1,50 @@
 import React from "react";
+import loginService from "../services/login";
 
 const LoginForm = ({
-  handleSubmit,
-  handleUsernameChange,
-  handlePasswordChange,
   username,
+  setUsername,
   password,
+  setPassword,
+  setUser,
+  setMessage,
 }) => {
+  const handleLogin = async (event) => {
+    event.preventDefault();
+
+    try {
+      const user = await loginService.login({
+        username,
+        password,
+      });
+      window.localStorage.setItem("loggedBlogappUser", JSON.stringify(user));
+      setUser(user);
+      setUsername("");
+      setPassword("");
+    } catch (exception) {
+      setMessage("Wrong credentials");
+      setTimeout(() => {
+        setMessage(null);
+      }, 5000);
+    }
+  };
+
   return (
     <div>
-      <h2>Login</h2>
-
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleLogin}>
         <div>
           username
-          <input value={username} onChange={handleUsernameChange} />
+          <input
+            value={username}
+            onChange={({ target }) => setUsername(target.value)}
+          />
         </div>
         <div>
           password
           <input
             type="password"
             value={password}
-            onChange={handlePasswordChange}
+            onChange={({ target }) => setPassword(target.value)}
           />
         </div>
         <button type="submit">login</button>
@@ -30,4 +53,4 @@ const LoginForm = ({
   );
 };
 
-export default LoginForm;
+export { LoginForm };
