f9acbea4088f247b981e56b2a53dacab4e941e01
Konstantin <95539196+EalsyCoD@users.noreply.github.com>
2022-01-26 19:43:01 +0300

    Exercises 6.13-6.14

--- part6/anecdotes-redux/src/App.js
+++ part6/anecdotes-redux/src/App.js
@@ -1,10 +1,21 @@
-import React from 'react'
-import {AnecdoteForm} from './components/AnecdoteForm'
-import {AnecdoteList} from './components/AnecdoteList'
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { AnecdoteForm } from './components/AnecdoteForm'
+import { AnecdoteList } from './components/AnecdoteList'
 import { VisibilityFilter } from './components/VisibilityFilter'
 import { Notification } from './components/Notification'
 
+
+
+import { getAll } from './service/anecdotes'
+import { initAnecdotes } from './reducers/anecdoteReducer'
+
+
 const App = () => {
+  const dispatch = useDispatch()
+  useEffect(() => {
+      getAll().then(anecdotes => dispatch(initAnecdotes(anecdotes)))
+  }, [dispatch])
   return (
     <div>
       <h2>Anecdotes</h2>
