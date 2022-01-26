1a8b0d1c468abe11ac0cb2fb8070512042f48a22
Konstantin <95539196+EalsyCoD@users.noreply.github.com>
2022-01-26 18:44:49 +0300

    Exercises 6.9-6.12

--- part6/anecdotes-redux/src/App.js
+++ part6/anecdotes-redux/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import {AnecdoteForm} from './components/AnecdoteForm'
 import {AnecdoteList} from './components/AnecdoteList'
+import { VisibilityFilter } from './components/VisibilityFilter'
+import { Notification } from './components/Notification'
 
 const App = () => {
   return (
     <div>
       <h2>Anecdotes</h2>
+      <Notification />
+      <VisibilityFilter/>
       <AnecdoteList />
       <AnecdoteForm />
     </div>
